8b3f34ef67896873cf3935a7296d222f0e09dbe6
Marco Facecchia <facecchiamarco@icloud.com>
2026-03-28 21:23:35 +0100

    feat: add user Specification builder implementation

---
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__/features/user/specification/UserSpecificationBuilder.java
@@ -0,0 +1,7 @@
+package ${package}.${artifactId}.features.user.specification;
+
+import ${package}.${artifactId}.features.user.data.User;
+import ${package}.${artifactId}.common.specification.CommonSpecificationBuilder;
+
+public class UserSpecificationBuilder extends CommonSpecificationBuilder<User> {
+}
